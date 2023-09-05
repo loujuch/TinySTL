16eeae11b209726180c5024b2fb103e5ce8007ad
loujuch <loujuch@ssh.com>
2023-09-05 15:53:17 +0800

    multi_set

---
+++ test/test_multi_set.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+
+#include "set.hpp"
+
+void show(const stl::MultiSet<int> &set_) {
+	std::cout << "size: " << set_.size() << std::endl;
+	for(auto &p : set_) {
+		std::cout << p << ' ';
+	}
+	std::cout << std::endl << std::endl;
+}
+
+void main_func() {
+	stl::MultiSet<int> set0;
+
+	set0.emplace();
+	show(set0);
+	set0.insert(1);
+	show(set0);
+	set0.insert(2);
+	show(set0);
+	set0.insert(1);
+	show(set0);
+	set0.insert(-1);
+	show(set0);
+	set0.insert(2);
+	show(set0);
+
+	int n = -1;
+	set0.insert(n);
+	show(set0);
+
+	set0.erase(2);
+	show(set0);
+
+	std::cout << set0.count(0) << std::endl;
+	std::cout << set0.count(3) << std::endl;
+
+	auto set1 = set0;
+	show(set1);
+	auto set2 = std::move(set0);
+	show(set2);
+
+	set2.insert(1120201453);
+	show(set2);
+
+	set1 = set2;
+	show(set1);
+
+	set2.insert(1120201454);
+	show(set2);
+
+	set1 = std::move(set2);
+	show(set1);
+	show(set2);
+
+	std::cout << set1.count(1) << std::endl;
+}
+
+int main() {
+	main_func();
+	return 0;
+}
